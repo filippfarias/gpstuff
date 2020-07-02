2854463e343b4094932b41d5459969f0fdf9f8ee
Filipe Farias <filipepfarias@MacBook-Air.local>
2020-07-01 23:03:53 -0300

    - Last sync;

--- gp/demo_spatial1.m
+++ gp/demo_spatial1.m
@@ -54,7 +54,7 @@
 %    See also  DEMO_REGRESSION1, DEMO_CLASSIFIC1
 %
 %  References:
-%    Vanhatalo, J., Pietiläinen V. and Vehtari, A. (2010). 
+%    Vanhatalo, J., Pietil??inen V. and Vehtari, A. (2010). 
 %    Approximate inference for disease mapping with sparse Gaussian
 %    processes. Statistics in Medicine, 29(15):.
 %
